5059d026d0a1efda437e2f16504b3ce525044430
jlnshly <jelaineashleyramos@gmail.com>
2026-03-12 20:27:08 +0800

    add print equal when numbers are the same

---
+++ equal_if_same_number.py
@@ -0,0 +1,10 @@
+#enter first number
+number_1 = float(input("Enter the first number: "))
+#enter second number
+number_2 = float(input("Enter the second number: "))
+
+#check if the two numbers are equal
+if number_1 == number_2:
+    print(f"{number_1} is equal to {number_2}.")
+else:
+    print(f"{number_1} is not equal to {number_2}.")
