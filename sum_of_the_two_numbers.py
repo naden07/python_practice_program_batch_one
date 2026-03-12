bf2b34304de696669815e841950393574acd46be
jlnshly <jelaineashleyramos@gmail.com>
2026-03-12 21:01:20 +0800

    add program for sum of two numbers

---
+++ sum_of_the_two_numbers.py
@@ -0,0 +1,7 @@
+#Enter first number
+number_1 = float(input("Enter a number: "))
+#Enter second number
+number_1 += float(input("Enter another number: "))
+
+#output
+print(f"The sum of the two numbers is {number_1}")
