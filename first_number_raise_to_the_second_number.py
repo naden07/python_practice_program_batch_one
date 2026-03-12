c78e6d58a8134da2997dc26ae01b4e96daef5611
jlnshly <jelaineashleyramos@gmail.com>
2026-03-12 21:22:12 +0800

    program done

---
+++ first_number_raise_to_the_second_number.py
@@ -0,0 +1,8 @@
+#Input first number
+number_1 = float(input("Enter another number: "))
+#Input second number
+number_2 = float(input("Enter another number: "))
+
+#Output
+print(f"Result: {number_1 ** number_2}")
+
