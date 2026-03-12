7f09ecacde1d8e29dcc277d2489e8b4b04a117c8
jlnshly <jelaineashleyramos@gmail.com>
2026-03-12 20:30:27 +0800

    program added :p

--- print_the_bigger_number.py
+++ print_the_bigger_number.py
@@ -1,5 +1,6 @@
-#input
+#enter the first number
 number_1 = float(input("Enter a number: "))
+#enter the second number
 number_2 = float(input("Enter another number: "))
 #output
 print(f"The bigger number is {max(number_1, number_2)}")
