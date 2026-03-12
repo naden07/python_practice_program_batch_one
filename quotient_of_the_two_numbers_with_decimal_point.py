5da8962ed17d410d94ed020c63b6578ff012d911
jlnshly <jelaineashleyramos@gmail.com>
2026-03-12 21:14:55 +0800

    quotient of the two numbers with decimal point doneeezz

---
+++ quotient_of_the_two_numbers_with_decimal_point.py
@@ -0,0 +1,8 @@
+#Enter first number
+number_1 = float(input("Enter the first number: "))
+#Enter second number
+number_1 /= float(input("Enter the second number: "))
+
+#Output
+print(f"The quotient of the two numbers is {number_1}")
+
